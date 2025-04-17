7f69266ad1ba18623b4c784f2d2fe51730181d1a
AA Python <aapythonletscode@gmail.com>
2025-04-17 15:02:50 -0700

    Initial Commit

---
+++ Wellness/WellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessApp.swift
+//  Wellness
+//
+//  Created by Aarnav Dhir on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct WellnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WellnessTests/WellnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  WellnessTests.swift
+//  WellnessTests
+//
+//  Created by Aarnav Dhir on 4/17/25.
+//
+
+import Testing
+@testable import Wellness
+
+struct WellnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
