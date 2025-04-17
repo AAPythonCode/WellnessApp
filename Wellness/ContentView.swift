707243d167544759d74abb085bd5596625686aec
AA Python <aapythonletscode@gmail.com>
2025-04-17 15:36:31 -0700

    Styled

--- Wellness/ContentView.swift
+++ Wellness/ContentView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("Welcome to the Wellness App")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .bold()
+                .foregroundColor(.white)
+                .offset(x: -30, y: -190)
             Image(systemName: "globe")
                 .resizable()
                 .foregroundColor(.white)
@@ -18,6 +24,8 @@ struct ContentView: View {
                 .offset(x: -110, y: -190)
         }
         .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.blue)
     }
 }
 
