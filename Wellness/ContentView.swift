5b3a7dcd437e44072e3d8b332929503a7b455b2b
AA Python <aapythonletscode@gmail.com>
2025-04-18 15:33:20 -0700

    Added UI Screen

--- Wellness/ContentView.swift
+++ Wellness/ContentView.swift
@@ -8,27 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var email: String
     var body: some View {
         VStack {
             Text("Welcome to the Wellness App")
                 .font(.largeTitle)
                 .fontWeight(.black)
+                .multilineTextAlignment(.center)
                 .bold()
                 .foregroundColor(.white)
-                .offset(x: -30, y: -190)
-            Image(systemName: "globe")
+                .offset(y: -120)
+                .font(Font.custom(
+                    "Gill Sans", size: 27
+                ))
+            Image(systemName: "suit.heart.fill")
                 .resizable()
                 .foregroundColor(.white)
-                .frame(width: 100, height: 100)
+                .frame(
+                    width: 100,
+                    height: 100
+                )
                 .scaledToFill()
-                .offset(x: -110, y: -190)
+                .offset(y: -120)
+            Text("Log in")
+                .font(Font.custom(
+                    "Gill Sans Light", size: 27
+                ))
+                .foregroundStyle(.blue)
+
+                .offset(y: -100)
+            TextField("Enter email", text: $email)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.black)
+                .frame(width: 380)
+                .offset(x: 0, y: -55)
+                .textFieldStyle(.roundedBorder)
+                .font(Font.custom(
+                    "Gill Sans Light", size: 27
+                ))
+            TextField("Enter password", text: $email)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.black)
+                .frame(width: 380)
+                .offset(x: 0, y: -50)
+                .textFieldStyle(.roundedBorder)
+                .font(Font.custom(
+                    "Gill Sans Light", size: 27
+                ))
+            Button(action: signIn) {
+                Label("Sign In", systemImage: "arrow.up")
+            }
+            .padding()
+            .foregroundStyle(.green)
+            .background(.blue)
+            .cornerRadius(32)
+            Button(action: signIn) {
+                Label("... or sign up", systemImage: "none")
+            }
+            .offset(y: 22)
+            .offset(x: -9)
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.blue)
+        .background(.green)
+    }
+    func signIn() {
+        print("hello")
     }
 }
 
 #Preview {
-    ContentView()
+    ContentView(email: "")
 }
--- Wellness/WellnessApp.swift
+++ Wellness/WellnessApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct WellnessApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView(email: "")
         }
     }
 }
