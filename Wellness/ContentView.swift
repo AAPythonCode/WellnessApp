f6e4b874cad9d91d61817cc78102502516e1d42a
AA Python <aapythonletscode@gmail.com>
2025-04-19 13:34:08 -0700

    added package dependencies

--- Wellness/ContentView.swift
+++ Wellness/ContentView.swift
@@ -1,9 +1,11 @@
-
-
 import SwiftUI
+import FirebaseAuth
 
 struct ContentView: View {
     @State var email: String
+    @State var password: String
+    @State var logIn: Bool = false
+
     var body: some View {
         VStack {
             Text("Welcome to the Wellness App")
@@ -13,65 +15,79 @@ struct ContentView: View {
                 .bold()
                 .foregroundColor(.white)
                 .offset(y: -120)
-                .font(Font.custom(
-                    "Gill Sans", size: 27
-                ))
+                .font(Font.custom("Gill Sans", size: 27))
+
             Image(systemName: "suit.heart.fill")
                 .resizable()
                 .foregroundColor(.white)
-                .frame(
-                    width: 120,
-                    height: 100
-                )
+                .frame(width: 120, height: 100)
                 .scaledToFill()
                 .offset(y: -120)
-            Text("Log in")
-                .font(Font.custom(
-                    "Gill Sans Light", size: 27
-                ))
-                .foregroundStyle(.blue)
 
+            Text("Log in")
+                .font(Font.custom("Gill Sans Light", size: 27))
+                .foregroundStyle(.blue)
                 .offset(y: -100)
+
             TextField("Enter email", text: $email)
                 .multilineTextAlignment(.center)
                 .foregroundStyle(.black)
                 .frame(width: 380)
-                .offset(x: 0, y: -55)
+                .offset(y: -55)
                 .textFieldStyle(.roundedBorder)
-                .font(Font.custom(
-                    "Gill Sans Light", size: 27
-                ))
-            TextField("Enter password", text: $email)
+                .font(Font.custom("Gill Sans Light", size: 27))
+
+            TextField("Enter password", text: $password)
                 .multilineTextAlignment(.center)
                 .foregroundStyle(.black)
                 .frame(width: 380)
-                .offset(x: 0, y: -50)
+                .offset(y: -50)
                 .textFieldStyle(.roundedBorder)
-                .font(Font.custom(
-                    "Gill Sans Light", size: 27
-                ))
+                .font(Font.custom("Gill Sans Light", size: 27))
+
             Button(action: signIn) {
-                Label("Sign In", systemImage: "arrow.up")
+                Label("Log In", systemImage: "arrow.up")
             }
             .padding()
             .foregroundStyle(.green)
             .background(.blue)
             .cornerRadius(32)
-            Button(action: signIn) {
+
+            Button(action: resetPassword) {
+                Label("Forgot Password?", systemImage: "person.badge.key")
+            }
+            .offset(y: 20)
+            Button {
+                logIn = true
+            } label: {
                 Label("...or sign up", systemImage: "none")
             }
-            .offset(y: 22)
+            .offset(y: 50)
             .offset(x: -9)
         }
         .padding()
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.green)
+        .fullScreenCover(isPresented: $logIn) {
+            SignUpView(email: "", password: "", reenter: "", logIn: $logIn)
+        }
     }
+
     func signIn() {
-        print("hello")
+        self.logIn.toggle()
+    }
+
+    func resetPassword() {
+        Auth.auth().sendPasswordReset(withEmail: email) { error in
+            if let error = error {
+                print("Password reset error: \(error.localizedDescription)")
+            } else {
+                print("Password reset email sent to \(email)")
+            }
+        }
     }
 }
 
 #Preview {
-    ContentView(email: "")
+    ContentView(email: "", password: "")
 }
--- Wellness/SignUpView.swift
+++ Wellness/SignUpView.swift
@@ -1,18 +1,134 @@
-//
-//  SwiftUIView.swift
-//  Wellness
-//
-//  Created by Aarnav Dhir on 4/18/25.
-//
-
 import SwiftUI
+import FirebaseAuth
 
-struct SwiftUIView: View {
+struct SignUpView: View {
+    @State var email: String
+    @State var password: String
+    @State var reenter: String
+    @State private var signUpErrorMessage: String = ""
+    @Binding var logIn: Bool
+    
+    var isFormValid: Bool {
+        return !email.isEmpty &&
+               password.count > 3 &&
+               password.count <= 20 &&
+               password == reenter
+    }
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Welcome to the Wellness App")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .multilineTextAlignment(.center)
+                .bold()
+                .foregroundColor(.white)
+                .font(Font.custom(
+                    "Gill Sans", size: 27
+                ))
+            Image(systemName: "suit.heart.fill")
+                .resizable()
+                .foregroundColor(.white)
+                .frame(
+                    width: 120,
+                    height: 100
+                )
+                .scaledToFill()
+            Text("Sign Up")
+                .font(Font.custom(
+                    "Gill Sans Light", size: 27
+                ))
+                .foregroundStyle(.green)
+
+            TextField("Enter email", text: $email)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.black)
+                .frame(width: 380)
+                .textFieldStyle(.roundedBorder)
+                .font(Font.custom(
+                    "Gill Sans Light", size: 27
+                ))
+            SecureField("Enter password", text: $password)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.black)
+                .frame(width: 380)
+                .textFieldStyle(.roundedBorder)
+                .font(Font.custom(
+                    "Gill Sans Light", size: 27
+                ))
+            
+            SecureField("Re-enter password", text: $reenter)
+                .multilineTextAlignment(.center)
+                .foregroundStyle(.black)
+                .frame(width: 380)
+                .textFieldStyle(.roundedBorder)
+                .font(Font.custom(
+                    "Gill Sans Light", size: 27
+                ))
+            
+            validationMessage()
+
+            Button(action: signUp) {
+                Label("Sign Up", systemImage: "arrow.up")
+            }
+            .padding()
+            .foregroundStyle(.blue)
+            .background(isFormValid ? Color.green : Color.gray)
+            .cornerRadius(32)
+            .disabled(!isFormValid)
+            if !signUpErrorMessage.isEmpty {
+                Text(signUpErrorMessage)
+                    .font(Font.custom("Gill Sans Light", size: 20))
+                    .foregroundStyle(.green)
+            }
+            Button(action: logInBack) {
+                Label("...or log in", systemImage: "none")
+            }
+        }
+        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.blue)
+    }
+    
+    @ViewBuilder
+    func validationMessage() -> some View {
+        if password.count < 1 {
+            Text("Please enter a password.")
+                .font(Font.custom("Gill Sans Light", size: 20))
+                .foregroundStyle(.green)
+        } else if password.count <= 3 {
+            Text("Password should be more than 6 characters.")
+                .font(Font.custom("Gill Sans Light", size: 20))
+                .foregroundStyle(.green)
+        } else if password.count > 20 {
+            Text("Password should be less than 20 characters.")
+                .font(Font.custom("Gill Sans Light", size: 20))
+                .foregroundStyle(.green)
+        } else if reenter != password {
+            Text("Passwords do not match.")
+                .font(Font.custom("Gill Sans Light", size: 20))
+                .foregroundStyle(.green)
+        }
+
+    }
+    
+    func logInBack() {
+        self.logIn.toggle()
+        print("hi")
+    }
+    func signUp() {
+        print("Sign up button tapped")
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                signUpErrorMessage = "Sign Up Error: \(error.localizedDescription)"
+                
+            } else {
+                print("Account created for: \(result?.user.email ?? "unknown email")")
+                self.logIn.toggle()
+            }
+        }
     }
 }
 
 #Preview {
-    SwiftUIView()
+    SignUpView(email: "", password: "", reenter: "", logIn: .constant(false))
 }
